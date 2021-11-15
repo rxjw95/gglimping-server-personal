1e458c6fbe01e509fa27b96016835d3e0b579960
Jangwook Ryu <rxjw95@gmail.com>
2021-11-16 00:47:45 +0900

    Add fetch camp feature

--- src/main/java/com/gglimping/gglim/camp/service/CampService.java
+++ src/main/java/com/gglimping/gglim/camp/service/CampService.java
@@ -6,7 +6,10 @@ import com.gglimping.gglim.camp.dtos.ResponseFetchCamps;
 import com.gglimping.gglim.camp.repository.CampEntity;
 import com.gglimping.gglim.camp.repository.CampRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,10 +20,15 @@ public class CampService {
     private final CampRepository campRepository;
     private final CampAssembler campAssembler;
 
+    //TODO: need join for price
     public List<ResponseFetchCamps> fetchCampsBy(Long lastCampId, int size) {
+        PageRequest pageRequest = PageRequest.of(0, size);
+        Page<CampEntity> camps = campRepository.findByCampIdLessThanOrderByCampIdDesc(lastCampId, pageRequest);
+        //FIXME: Need CampEntity to Dtos(ResponseBasicCampInfo) logic
         return null;
     }
 
+    @Transactional(readOnly = true)
     public ResponseCamp getCampBy(Long id) throws Exception {
         Optional<CampEntity> campEntity = campRepository.findById(id);
         return campAssembler.toCampDto(campEntity);
